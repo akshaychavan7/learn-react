75bee30a4b6178da5542245b35611f35c5557bc1
= <=>
2024-02-14 16:16:50 -0500

    update objects form example

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,11 +2,11 @@
 // import Image from 'next/image'
 // import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
-import RequestTracker from './qcomps/shoppingCart'
+import Scoreboard from './qcomps/updObjectsForm'
 export default function Home() {
   return (
     <div className={styles.main}>
-        <RequestTracker />
+        <Scoreboard />
     </div>
   )
 }
