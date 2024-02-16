715c4b8040bceabd56e7249998740fbf5941e16c
= <=>
2024-02-16 16:03:39 -0500

    recipe list example

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,11 +2,11 @@
 // import Image from 'next/image'
 // import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
-import List from './qcomps/list_keys_id'
+import RecipeList from './qcomps/recipes'
 export default function Home() {
   return (
     <div className={styles.main}>
-        <List />
+        <RecipeList />
     </div>
   )
 }
