744dbf5650b98a262bdb9b15097d7bc5cb0e47b1
= <=>
2024-02-16 16:59:02 -0500

    state examples

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,11 +2,11 @@
 // import Image from 'next/image'
 // import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
-import RecipeList from './qcomps/recipes'
+import Form from './qcomps/stuckForm'
 export default function Home() {
   return (
     <div className={styles.main}>
-        <RecipeList />
+        <Form />
     </div>
   )
 }
