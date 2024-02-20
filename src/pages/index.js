f9fcd5383081d8fdadca06536241318060088a6b
= <=>
2024-02-20 15:52:05 -0500

    update object example

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,11 +2,11 @@
 // import Image from 'next/image'
 // import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
-import Form from './qcomps/stuckForm'
+import Scoreboard from './qcomps/updObjectsForm'
 export default function Home() {
   return (
     <div className={styles.main}>
-        <Form />
+        <Scoreboard />
     </div>
   )
 }
